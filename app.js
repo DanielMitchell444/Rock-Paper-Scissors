b6430966bf05ed2da59e7fc43c5b381a7a006783
Daniel Mitchell <49624246+DanielMitchell444@users.noreply.github.com>
2022-11-19 20:09:13 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 
-const buttons = document.querySelector('.button');
+const buttons = document.querySelectorAll('.button');
+
+const text = document.querySelector('.text')
+
 
 
 const getComputerChoice = () => {
@@ -7,20 +10,26 @@ const getComputerChoice = () => {
     var computerChoice = Math.random();
 
     if(computerChoice <= 0.34){
-        console.log('rock');
-        return "rock"
+        
+        return "Rock"
     }
 
     else if(computerChoice <= 0.68){
      console.log('paper')
-     return "paper"
+     return "Paper"
     }
 
     else{
      console.log('scissors');
-     return "scissors";
+     return "Scissors";
 
     }
+}
+
+
+const getUserChoice = (e) => {
+
+
 }
 
 
@@ -28,22 +37,56 @@ const getComputerChoice = () => {
 
 
 
-const playGame = (userInput, computerInput) => {
+const playGame = (computerSelection, playerSelection) => {
 
-        if (userInput === 'rock' && computerInput === 'paper') {
-          console.log('You lose! paper beats rock');
-        } else if (userInput === 'rock' && computerInput === 'scissors') {
-          console.log('You win! rock  beats scissors');
+      if (computerSelection === "Rock" && playerSelection === "Scissors") {
+    return "Computer wins";
+  } else if (computerSelection === "Paper" && playerSelection === "Rock") {
+    return "Computer Wins";
+  } else if (computerSelection === "Scissors" && playerSelection === "Paper") {
+    return "Computer wins";
+  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    return "Computer wins";
+  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    return "Player wins";
+  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    return "Player wins";
 
-        } else if(userInput === 'paper' && computerInput === "rock") {
-        } else {
-          console.log('You tie!');
-        }
-      }
+  } else if(playerSelection === "Rock" && computerSelection === "Rock"){
+    return "Tie"
+
+  } else if(playerSelection === "Paper" && computerSelection === "Paper"){
+    return "tie"
+  }
+}
 
 
-playerSelection = 'rock';
+const game = () => {
+  for(let i = 0; i < 5; i++){
+    pla
+  }
+}
+
+
+
+
+playerSelection = prompt('please enter rock, paper, scissors');
 
 const computerChoice = getComputerChoice();
 
 console.log(playGame(playerSelection, computerChoice))
+
+console.log(playerSelection);
+
+
+
+buttons.forEach(item => {
+ item.addEventListener('click', (e) => {
+  let currentTarget = e.target;
+  let CurrentValue = e.target.dataset.selection;
+  
+  if(CurrentValue === "rock"){
+    console.log('rock');
+  }
+ })
+})
